a4e193985727e2217131739730a2474654e90eca
aliang30 <aliang30@binghamton>
2019-11-04 14:02:45 -0500

    Added new activities + made button modifications

--- app/src/main/java/com/example/cs441_project5/Main3Activity.java
+++ app/src/main/java/com/example/cs441_project5/Main3Activity.java
@@ -7,41 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
-    private Button startButton;
-    private Button secondButton;
+    private Button retry;
+    private Button mainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
 
-        startButton = (Button) findViewById(R.id.button);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        retry = (Button) findViewById(R.id.button);
+        retry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity();
             }
         });
 
-        secondButton = (Button) findViewById(R.id.button2);
-        secondButton.setOnClickListener(new View.OnClickListener() {
+        mainMenu = (Button) findViewById(R.id.button2);
+        mainMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity2();
             }
         });
-
     }
 
+    //Opens main menu screen
     public void openActivity() {
         Intent intent = new Intent (this, Main2Activity.class);
         startActivity(intent);
     }
 
+    //Opens game screen
     public void openActivity2() {
-        Intent intent = new Intent (this, Instructions.class);
+        Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
     }
 }
