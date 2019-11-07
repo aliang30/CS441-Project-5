51f471d999e5379a95ca7a7573fc7f8850388de2
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-11-06 22:34:53 -0500

    add code

--- app/src/main/java/com/example/cs441_project5/Instructions.java
+++ app/src/main/java/com/example/cs441_project5/Instructions.java
@@ -1,11 +1,11 @@
 package com.example.cs441_project5;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class Instructions extends AppCompatActivity {
 
